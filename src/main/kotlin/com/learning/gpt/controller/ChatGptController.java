4afc02dba9de89c59bb2d49568c91e9676801325
SoohoLee <hyeri0603@naver.com>
2024-01-16 19:32:56 +0900

    api-key 제거

--- src/main/kotlin/com/learning/gpt/controller/ChatGptController.java
+++ src/main/kotlin/com/learning/gpt/controller/ChatGptController.java
@@ -1,20 +1,16 @@
 package com.learning.gpt.controller;
 
-import com.learning.gpt.dto.*;
+import com.learning.gpt.dto.GptModelsResponseDto;
+import com.learning.gpt.dto.GptResponseDto;
 import com.learning.gpt.service.ChatGptService;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ai/gpt")
